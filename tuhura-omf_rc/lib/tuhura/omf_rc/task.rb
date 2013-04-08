8d6d03fac58f6af732bd78b5f7cad2a2da350ee6
Max Ott <max.ott@nicta.com.au>
2013-04-08 18:45:19 +1000

    Still struggling with rvm package

--- tuhura-omf_rc/lib/tuhura/omf_rc/task.rb
+++ tuhura-omf_rc/lib/tuhura/omf_rc/task.rb
@@ -97,7 +97,8 @@ module Tuhura::OmfRc
                 res.change_state :installing
                 @tmpdir = File.join(Dir.tmpdir, SecureRandom.uuid)
                 Dir.mkdir(@tmpdir)
-                cmd = "cd #{@tmpdir}; tar zxf #{state[:path]}; /usr/local/rvm/bin/rvm jruby exec bundle package --all 2>&1"
+                #cmd = "cd #{@tmpdir}; tar zxf #{state[:path]}; /usr/local/rvm/bin/rvm jruby exec bundle package --all 2>&1"
+                cmd = "bash -x #{File.dirname(__FILE__)}/../../sbin/prepare_task.sh 2>&1"
                 debug "Executing '#{cmd}'"
                 ExecApp.new('preparing', cmd, true, @tmpdir) do |event_type, app_id, msg|
                   debug "#{event_type}:: #{msg}"
---
+++ tuhura-omf_rc/sbin/prepare_task.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Install task software and initialise ruby environment
+#
+# usage: tarfile tmpdir ruby_ver
+#
+TAR_FILE=$1
+TMP_DIR=$2
+RUBY_VER=$3
+
+mkdir $TMP_DIR
+cd $TMP_DIR
+tar zxf #$TAR_FILE
+source /usr/local/rvm/environments/$RUBY_VER
+bundle package --all
