ced0c4571d6c569cfad73369747c172d62db5151
Max Ott <max.ott@nicta.com.au>
2013-04-09 12:26:28 +1000

    Need to make sure maven is installed as well.

--- tuhura-omf_rc/sbin/prepare_task.sh
+++ tuhura-omf_rc/sbin/prepare_task.sh
@@ -15,6 +15,14 @@ if [ ! -e /usr/local/rvm/environments/$RUBY_VER ]; then
   echo "STATUS: installing.${RUBY_VER}"
   /usr/local/rvm/bin/rvm install --autolibs=4 $RUBY_VER
 fi
+
+# check if maven is installed
+command -v mvn > /dev/null
+if [ $? != 0 ]; then
+  echo "STATUS: installing.maven"
+  apt-get install maven2
+fi
+
 echo "STATUS: installing.gems"
 source /usr/local/rvm/environments/$RUBY_VER
 gem install bundler
